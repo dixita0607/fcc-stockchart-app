4fdd09468c4d96d8d19bb4db343c8bbf24328f92
Dixita Ganatra <dixita0607@gmail.com>
2018-01-16 19:27:04 +0530

    Correct the order of routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 
 const routes: Routes = [
-  {path: '**', redirectTo: 'home'},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: '**', redirectTo: 'home'}
 ];
 
 @NgModule({
